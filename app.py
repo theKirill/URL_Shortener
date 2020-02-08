46a5c10e5391a02bc3002f188b53c858ba23314d
theKirill <k.a.yanushkin@gmail.com>
2020-02-08 13:45:39 +0300

    fixed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, json, make_response, Response
+from flask import Flask, request, json, make_response
 from shorten import URL_Shortener
 
 app = Flask(__name__)
@@ -6,31 +6,45 @@ shortener = URL_Shortener()
 
 @app.route('/urls', methods=['post'])
 def get_short_url():
+    res = "{'success': False}"
+    status = 400
+
     if request.json:
         long_url = request.json['long_url']
         success, id = shortener.short(long_url)
 
         if success:
-            response = "{" + "'success': True, 'id': '{}'".format(id) + "}"
+            res = "{" + "'success': True, 'id': '{}'".format(id) + "}"
             status = 201
         else:
-            response = "{'success': False}"
             status = 500
-            
-        return Response(response, status = status, mimetype = 'application/json')
 
-    return Response("{'success': False}", status = 400, mimetype = 'application/json')
+    response = make_response(json.dumps(res))
+    response.headers['Content-Type'] = 'application/json'
+    return response, status
 
 @app.route('/urls', methods=['get'])
 def get_long_url():
     id = request.args.get('id', 'null')
-    success, long_url = shortener.get_long_url(id)
+
+    if id == 'null':
+        success = False
+    else:
+        success, long_url = shortener.get_long_url(id)
     
     if success:
-        response = "{'success': True, " + "'long_url': '{}'".format(long_url) + "}"
+        res = "{'success': True, " + "'long_url': '{}'".format(long_url) + "}"
         status = 200
     else:
-        response = "{'success': False}"
-        status = 500
-    
-    return Response(response, status = status, mimetype = 'application/json')
+        res = "{'success': False}"
+        if id == 'null':
+            status = 400
+        else:
+            status = 500
+
+    response = make_response(json.dumps(res))
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Cache-Control'] = 'no-cache'    
+    response.headers['Pragma'] = 'no-cache'    
+
+    return response, status
--- shorten.py
+++ shorten.py
@@ -8,22 +8,24 @@ class URL_Shortener:
                                        port=6379,
                                        db=0)
 
-    def __get_short_id(self, url):
+    def __get_short_id(self):
         id = "".join(random.sample('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 5))
         return id
 
-    def short(self, long_url):
-        id = self.__get_short_id(long_url)
-        short_url = "http://127.0.0.1:5000/urls/{}".format(id)
-
+    def __save_to_db(self, id, long_url):
         try:
             self.redis.set(id, long_url)
             return True, id
         except:
-            return False
+            return False, 'null'
+
+    def short(self, long_url):
+        id = self.__get_short_id()
+
+        return self.__save_to_db(id, long_url)
 
     def get_long_url(self, id):
         try:
             return True, self.redis.get(id)
         except:
-            return False
+            return False, 'null'
